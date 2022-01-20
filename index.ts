5476e239bbc1df1d1a2ae77858093ec0b4c2c321
Vladymyr Zenin <vovan1608@gmail.com>
2022-01-20 13:39:55 +0200

    added CarType and Car class

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import Garage from './clases/Garage';
+import Car from './clases/Car';
 
 const garage = new Garage();
+const car = new Car();
+car.attach(garage);
 
-garage.toggleGate()
+garage.toggleGate();
 
 setTimeout(() => garage.toggleGate(), 4000);
 setTimeout(() => garage.toggleGate(), 5000);
+setTimeout(() => car.carIsComing(), 9000);
