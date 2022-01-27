d56a8be1b5250b9c3c6fd59fa3093811a5c3759b
Vladymyr Zenin <vovan1608@gmail.com>
2022-01-27 11:51:26 +0200

    added car class test

--- test/Car.test.ts
+++ test/Car.test.ts
@@ -1,3 +1,17 @@
 import Car from "../clases/Car";
 
-describe('', () => { });
+describe('test Car class', () => {
+	let car: Car;
+
+	beforeEach(() => {
+		car = new Car();
+	});
+
+	it('method attach adds observer into observers[]', () => {
+		const spy = jest.spyOn(car, 'attach');
+
+		car.attach('Bob');
+
+		expect(spy).toHaveBeenCalledWith('Bob');
+	});
+});
